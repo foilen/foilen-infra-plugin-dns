caecad10389b6ea4668ce217e613c7ae0ee66b5a
Simon Levesque <simon@foilen.com>
2018-09-26 21:53:47 -0400

    DnsEntryUpdateHandler - Improve performances

--- src/main/java/com/foilen/infra/resource/dns/DnsEntryUpdateHandler.java
+++ src/main/java/com/foilen/infra/resource/dns/DnsEntryUpdateHandler.java
@@ -9,29 +9,53 @@
  */
 package com.foilen.infra.resource.dns;
 
+import java.util.List;
+
 import com.foilen.infra.plugin.v1.core.common.DomainHelper;
 import com.foilen.infra.plugin.v1.core.context.ChangesContext;
 import com.foilen.infra.plugin.v1.core.context.CommonServicesContext;
-import com.foilen.infra.plugin.v1.core.eventhandler.AbstractCommonMethodUpdateEventHandler;
-import com.foilen.infra.plugin.v1.core.eventhandler.CommonMethodUpdateEventHandlerContext;
+import com.foilen.infra.plugin.v1.core.eventhandler.AbstractUpdateEventHandler;
+import com.foilen.infra.plugin.v1.model.resource.IPResource;
+import com.foilen.infra.plugin.v1.model.resource.LinkTypeConstants;
 import com.foilen.infra.resource.domain.Domain;
+import com.foilen.smalltools.tools.StringTools;
+import com.foilen.smalltools.tuple.Tuple3;
 
-public class DnsEntryUpdateHandler extends AbstractCommonMethodUpdateEventHandler<DnsEntry> {
+public class DnsEntryUpdateHandler extends AbstractUpdateEventHandler<DnsEntry> {
 
     @Override
-    protected void commonHandlerExecute(CommonServicesContext services, ChangesContext changes, CommonMethodUpdateEventHandlerContext<DnsEntry> context) {
+    public void addHandler(CommonServicesContext services, ChangesContext changes, DnsEntry dnsEntry) {
+        IPResource domain = retrieveOrCreateResource(services.getResourceService(), changes, new Domain(dnsEntry.getName(), DomainHelper.reverseDomainName(dnsEntry.getName())));
+        changes.linkAdd(dnsEntry, LinkTypeConstants.MANAGES, domain);
+    }
 
-        DnsEntry resource = context.getResource();
+    @Override
+    public void checkAndFix(CommonServicesContext services, ChangesContext changes, DnsEntry resource) {
+    }
 
-        context.getManagedResourceTypes().add(Domain.class);
-
-        context.getManagedResources().add(new Domain(resource.getName(), DomainHelper.reverseDomainName(resource.getName())));
-
+    @Override
+    public void deleteHandler(CommonServicesContext services, ChangesContext changes, DnsEntry resource, List<Tuple3<IPResource, String, IPResource>> previousLinks) {
+        detachManagedResources(services, changes, resource, previousLinks);
     }
 
     @Override
     public Class<DnsEntry> supportedClass() {
         return DnsEntry.class;
+    }
+
+    @Override
+    public void updateHandler(CommonServicesContext services, ChangesContext changes, DnsEntry previousResource, DnsEntry newResource) {
+        // If domain is different, detach and use another
+        if (!StringTools.safeEquals(previousResource.getName(), newResource.getName())) {
+            // Remove old
+            List<Domain> domains = services.getResourceService().linkFindAllByFromResourceAndLinkTypeAndToResourceClass(newResource, LinkTypeConstants.MANAGES, Domain.class);
+            removeManagedLinkAndDeleteIfNotManagedByAnyoneElse(services.getResourceService(), changes, domains, newResource);
+
+            // Add new
+            IPResource domain = retrieveOrCreateResource(services.getResourceService(), changes, new Domain(newResource.getName(), DomainHelper.reverseDomainName(newResource.getName())));
+            changes.linkAdd(newResource, LinkTypeConstants.MANAGES, domain);
+        }
+
     }
 
 }
---
+++ src/test/java/com/foilen/infra/resource/dns/DnsEntryTest.java
@@ -0,0 +1,70 @@
+/*
+    Foilen Infra Resource Infra Config
+    https://github.com/foilen/foilen-infra-resource-infraconfig
+    Copyright (c) 2018 Foilen (http://foilen.com)
+
+    The MIT License
+    http://opensource.org/licenses/MIT
+
+ */
+package com.foilen.infra.resource.dns;
+
+import org.junit.Test;
+
+import com.foilen.infra.plugin.core.system.fake.junits.AbstractIPPluginTest;
+import com.foilen.infra.plugin.core.system.junits.JunitsHelper;
+import com.foilen.infra.plugin.v1.core.context.ChangesContext;
+import com.foilen.infra.plugin.v1.core.service.IPResourceService;
+import com.foilen.infra.plugin.v1.core.service.internal.InternalChangeService;
+import com.foilen.infra.resource.dns.model.DnsEntryType;
+
+public class DnsEntryTest extends AbstractIPPluginTest {
+
+    @Test
+    public void test_basic() {
+
+        IPResourceService resourceService = getCommonServicesContext().getResourceService();
+        InternalChangeService internalChangeService = getInternalServicesContext().getInternalChangeService();
+
+        // Create some
+        ChangesContext changes = new ChangesContext(resourceService);
+        for (int i = 0; i < 5; ++i) {
+            changes.resourceAdd(new DnsEntry("d" + i + ".example.com", DnsEntryType.TXT, "hello"));
+        }
+        internalChangeService.changesExecute(changes);
+        JunitsHelper.assertState(getCommonServicesContext(), getInternalServicesContext(), "DnsEntryTest_test_basic-state-1.json", getClass(), true);
+
+        // Update some
+        DnsEntry dnsEntry = resourceService.resourceFindByPk(new DnsEntry("d1.example.com", DnsEntryType.TXT, "hello")).get();
+        changes.resourceUpdate(dnsEntry, new DnsEntry("d1.example.com", DnsEntryType.TXT, "hello 2"));
+        dnsEntry = resourceService.resourceFindByPk(new DnsEntry("d2.example.com", DnsEntryType.TXT, "hello")).get();
+        changes.resourceUpdate(dnsEntry, new DnsEntry("u2.example.com", DnsEntryType.TXT, "hello"));
+        internalChangeService.changesExecute(changes);
+        JunitsHelper.assertState(getCommonServicesContext(), getInternalServicesContext(), "DnsEntryTest_test_basic-state-2.json", getClass(), true);
+
+        // Remove some
+        changes.resourceDelete(resourceService.resourceFindByPk(new DnsEntry("d4.example.com", DnsEntryType.TXT, "hello")).get());
+        internalChangeService.changesExecute(changes);
+        JunitsHelper.assertState(getCommonServicesContext(), getInternalServicesContext(), "DnsEntryTest_test_basic-state-3.json", getClass(), true);
+
+    }
+
+    @Test
+    public void test_perf() {
+
+        IPResourceService resourceService = getCommonServicesContext().getResourceService();
+        InternalChangeService internalChangeService = getInternalServicesContext().getInternalChangeService();
+
+        // Create some
+        ChangesContext changes = new ChangesContext(resourceService);
+        for (int i = 0; i < 500; ++i) {
+            changes.resourceAdd(new DnsEntry("d" + i + ".example.com", DnsEntryType.TXT, "hello"));
+            if (i % 50 == 0) {
+                internalChangeService.changesExecute(changes);
+            }
+        }
+        internalChangeService.changesExecute(changes);
+
+    }
+
+}
